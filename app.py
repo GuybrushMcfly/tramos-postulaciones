cc669a936d42af09e2baf633525b6bd54a185e8d
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-04-14 19:22:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def tarjeta_gradiente_simple(titulo, valor, gradiente):
             box-shadow: 0px 4px 10px rgba(0,0,0,0.25);
             margin-bottom: 25px;
         ">
-            <div style="font-size: 25px; color: {color_texto}; font-weight: 700; text-shadow: {sombra_texto};">
+            <div style="font-size: 21px; color: {color_texto}; font-weight: 700; text-shadow: {sombra_texto};">
                 {titulo}
             </div>
             <div style="font-size: 37px; color: {color_texto}; font-weight: bold; text-shadow: {sombra_texto};">
