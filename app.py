c28492bea92b80768d38a5e21ddaf9609581033e
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-04-14 20:48:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from streamlit_echarts import st_echarts
 # ---- CONFIGURACIÓN DE PÁGINA ----
 st.set_page_config(page_title="Dashboard de Tramos", layout="wide")
 
-st.sidebar.image("logo capa.png", use_container_width=True)
+st.sidebar.image("logo-cap.png", use_container_width=True)
 
 modo = st.get_option("theme.base")
 color_texto = "#000000" if modo == "light" else "#FFFFFF"
