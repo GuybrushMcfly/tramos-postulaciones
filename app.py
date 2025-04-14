04ab015901ccc53d2e4299ba9b350b675fb092a7
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-04-14 15:28:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -301,24 +301,3 @@ st.markdown("<div style='margin-top: 60px;'></div>", unsafe_allow_html=True)
 with st.expander("🔎 VER DETALLES DE POSTULACIONES 🔎"):
     st.dataframe(postulaciones, use_container_width=True, hide_index=True)
 
-
-
-
-# Crear tabla de contingencia
-tabla = pd.crosstab(df["Dep. Nacional"], df["Nivel Post."])
-
-# Plot en matplotlib
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.heatmap(tabla, annot=True, fmt='d', cmap="YlOrBr", linewidths=0.5, linecolor='gray', cbar_kws={"label": "Cantidad"}, ax=ax)
-
-# Etiquetas y formato
-ax.set_title("Distribución por Departamento Nacional y Nivel", fontsize=14, weight="bold")
-ax.set_xlabel("Nivel Post.", fontsize=12)
-ax.set_ylabel("Dep. Nacional", fontsize=12)
-ax.tick_params(axis='x', rotation=0)
-ax.tick_params(axis='y', rotation=0)
-plt.tight_layout()
-
-# Mostrar en Streamlit
-st.pyplot(fig)
-
