1fb6507e805394349a5ea8a06f28f62992beea78
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-04-14 10:31:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def pie_chart_donut(df, columna, titulo, key_id):
         },
         "legend": {
             "orient": "horizontal",
-            "top": "84%",
+            "top": "83%",
             "bottom": "0%",
             "left": "center",
             "padding": [20, 0, 0, 0]  # margen superior de la leyenda
