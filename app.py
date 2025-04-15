9a26b2ed82718099ffeaf6a827e822561f3a6753
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-04-14 23:33:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -64,12 +64,12 @@ df = pd.DataFrame(data)
 valores = pd.DataFrame(sheet.worksheet("valores").get_all_records())
 
 # Limpieza de la columna "Monto"
-#valores["Monto"] = (
-#    valores["Monto"]
+valores["Monto"] = (
+    valores["Monto"]
 #    .astype(str)
 #    .str.replace(".", "", regex=False)
-    #.str.replace(",", ".", regex=False)
-#)
+    .str.replace(",", ".", regex=False)
+)
 #valores["Monto"] = pd.to_numeric(valores["Monto"], errors="coerce").fillna(0)
 
 # ---- FILTROS EN LA BARRA LATERAL ----
